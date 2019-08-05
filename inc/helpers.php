61b50e9a879f6257e9d2d2d7fecfd5625973e880
Galen Gidman <galen@galengidman.com>
2019-08-05 18:54:09 -0500

    cache query results

--- inc/helpers.php
+++ inc/helpers.php
@@ -17,11 +17,11 @@ function relatable_should_activate_admin() {
 }
 
 function relatable_get_to($channel, $post_id = null) {
-	$post_id = $post_id ?? get_the_ID();
-
 	global $wpdb;
 
-	$rows = $wpdb->get_results("
+	$post_id = $post_id ?? get_the_ID();
+
+	$rows = relatable_cached_query("
 		SELECT to_id
 		FROM {$wpdb->prefix}relatable
 		WHERE from_id = {$post_id}
@@ -34,11 +34,11 @@ function relatable_get_to($channel, $post_id = null) {
 }
 
 function relatable_get_from($channel, $post_id = null) {
-	$post_id = $post_id ?? get_the_ID();
-
 	global $wpdb;
 
-	$rows = $wpdb->get_results("
+	$post_id = $post_id ?? get_the_ID();
+
+	$rows = relatable_cached_query("
 		SELECT from_id
 		FROM {$wpdb->prefix}relatable
 		WHERE to_id = {$post_id}
@@ -48,4 +48,18 @@ function relatable_get_from($channel, $post_id = null) {
 	return array_map(function($r) {
 		return absint($r->from_id);
 	}, $rows);
+}
+
+function relatable_cached_query($sql, $method = 'get_results') {
+	global $wpdb;
+
+	$key = md5($sql);
+	$results = wp_cache_get($key, 'relatable');
+
+	if ($results === false) {
+		$results = $wpdb->{$method}($sql);
+		wp_cache_add($key, $results, 'relatable');
+	}
+
+	return $results;
 }
